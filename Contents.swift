9f77947076444e6454f7647b6f65f9f5483045af
Arzu95 <muhammadarzu3432@gmail.com>
2021-03-16 13:44:20 +0700

    Coba-Coba

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var str = "Hello, playground"
+// Variable
+var name = "Arzu"
+name = "Hmm"
+
+// Integer
+var uang = 1_000_000
+uang = 1_000_000_000
+
+// Multiline String
+var pantun = """
+bla bla bla bla
+bla bla bla bla
+bla bla
+"""
